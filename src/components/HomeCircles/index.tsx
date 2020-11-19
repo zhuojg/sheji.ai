bb142e077cc8e4c4cd034897ddce25dd09a881d1
zhuojg <zhuojg1519@outlook.com>
2020-11-19 19:34:46 +0800

    update homepage

---
+++ src/components/HomeCircles/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+const NavCircles = () => (
+    <div className={styles.circle_wrap}>
+        <div className={styles.circle_teaching}>
+            <div className={styles.circle_teaching_small}>
+
+            </div>
+        </div>
+        <div className={styles.circle_projects}>
+            <div className={styles.circle_projects_small}>
+
+            </div>
+        </div>
+        <div className={styles.circle_news}>
+            <div className={styles.circle_news_small}>
+
+            </div>
+        </div>
+    </div>
+)
+
+export default NavCircles
