338a58024cf3a1caef10d408bc04f4664fe50e39
zhuojg <zhuojg1519@outlook.com>
2020-11-21 15:37:00 +0800

    feature: going to eject

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,27 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './index.module.scss'
+import { Layout, Menu } from 'antd'
 import AboutUs from '../pages/AboutUs'
 import News from '../pages/News'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link, useLocation } from 'react-router-dom'
 
-const Layout = () => (
+const PageLayout = () => {
+  const { Header, Footer, Content } = Layout
+  const { SubMenu } = Menu
+  const [selectedMenu, setSelectedMenu] = useState(useLocation().pathname.replace('/', ''))
+
+  return (
     <div>
-        <div>
-            Header
-        </div>
-        <Router>
+      <Router>
+        <Layout className={styles.layout}>
+          <Header className={styles.header}>
+            <Menu mode="horizontal" theme="dark" selectedKeys={[selectedMenu]} onClick={(event) => {
+                // console.log(event.key)
+                setSelectedMenu(event.key.toString())
+            }}>
+              <Menu.Item key="news">
+                <Link to="/news">News</Link>
+              </Menu.Item>
+              <Menu.Item key="members">
+                <Link to="/members">Members</Link>
+              </Menu.Item>
+              <Menu.Item key="about">
+                <Link to="/about">About Us</Link>
+              </Menu.Item>
+            </Menu>
+          </Header>
+          <Content className={styles.content}>
             <Route path="/about">
-                <AboutUs />
+              <AboutUs />
             </Route>
             <Route path="/news">
-                <News />
+              <News />
             </Route>
-        </Router>
-
-        <div>
-            Footer
-        </div>
+          </Content>
+          <Footer className={styles.footer}>© 2020 从无限运算力到无限想象力. 沪ICP备17015556号-1</Footer>
+        </Layout>
+      </Router>
     </div>
-)
+  )
+}
 
-export default Layout
+export default PageLayout
---
+++ src/pages/JoinUs/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+const JoinUs = () => <div>Join Us</div>
+
+export default JoinUs
